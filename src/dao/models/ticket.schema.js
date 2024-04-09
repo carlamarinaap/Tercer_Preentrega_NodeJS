293bc2bc6459cdc2532cc646a6da1e2d2fae1e32
Carla Marina Apata <carla.apata@gmail.com>
2024-04-09 15:11:28 -0300

    Agrego uuid4 para generar el code del ticket

--- src/dao/models/ticket.schema.js
+++ src/dao/models/ticket.schema.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import uuid4 from "uuid4";
 
 const TicketSchema = new mongoose.Schema({
   code: {
     type: String,
     default: function () {
+      return uuid4();
       // Generar un código automático
-      return "COD" + Math.random().toString(36).substr(2, 6).toUpperCase();
+      // return "COD" + Math.random().toString(36).substr(2, 6).toUpperCase();
     },
   },
   purchase_datetime: {
